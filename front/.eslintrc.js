242afbd6f05edc2b489367fa31a5399a296f9ab1
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-05-10 07:31:32 +0900

    prettier,material-ui設定

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -2,25 +2,38 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
-  extends: ["plugin:react/recommended", "standard", "prettier"],
-  parser: "@typescript-eslint/parser",
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'standard',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint", "jest"],
+  plugins: [
+    'react',
+    'eslint-plugin-prettier',
+    '@typescript-eslint',
+    'jest',
+    'prettier',
+  ],
   rules: {
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
   },
-};
+}
